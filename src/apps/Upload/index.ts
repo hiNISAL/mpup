54425f440f7c2b16468ae66f90e62dfb7dc29c36
djy <dongjianye@nears.cn>
2020-05-26 17:34:39 +0800

    done 的 cmd 变成 uploadCmd

--- src/apps/Upload/index.ts
+++ src/apps/Upload/index.ts
@@ -165,7 +165,7 @@ export const Upload = async () => {
       stdout: uploadOut,
       config,
       abort,
-      cmd: uploadCmd,
+      uploadCmd,
     });
   } catch (e) {
     console.error(e);
